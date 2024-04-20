b8e6318dc3c758e807f3c1b2e250668f300ee5c1
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-21 03:21:41 +0600

    Added new template  function

--- functions.php
+++ functions.php
@@ -30,15 +30,7 @@ function medi_header()
     get_template_part('/inc/template-parts/header/header-1');
 }
 
-// medi_logo
-function medi_logo()
-{
-    $medi_logo = get_theme_mod('medi_header_logo', get_template_directory_uri() . '/assets/img/logo.png');
-?>
-    <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo esc_url($medi_logo); ?>" alt="<?php echo bloginfo(); ?>"></a>
-<?php
-}
-
 // include_once
 include_once('inc/common/scripts.php');
-include_once('inc/medi-kirki.php');
+include_once('inc/common/medi-kirki.php');
+include_once('inc/common/template-function.php');
